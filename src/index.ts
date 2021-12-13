5c3ef8b42b50580cd13d06151defc94b568d5103
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2021-12-13 20:29:39 -0300

    Removed dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-import shuffle = require("lodash.shuffle");
-import zip = require("lodash.zip");
+function shuffle<T>(array: T[]): T[] {
+  const result = array.slice();
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+  return result;
+}
+
+function zip<T, U>(a: T[], b: U[]): [T, U][] {
+  return a.map((x, i) => [x, b[i]]);
+}
 
 export default function genSecretFriends<T>(
   userIds: T[],
@@ -8,7 +18,7 @@ export default function genSecretFriends<T>(
   for (let i = 0; i < maxTries; i++) {
     const froms = shuffle(userIds);
     const tos = [...froms.slice(1), froms[0]];
-    const tuples = zip(froms, tos) as [T, T][];
+    const tuples = zip(froms, tos);
     const hasAnyFromEqualToTo = tuples.some(([from, to]) => from === to);
     if (!hasAnyFromEqualToTo) {
       return tuples.map(([from, to]) => ({ from, to }));
